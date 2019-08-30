d815efc83719d9e30864c73fc424ae6d3a8f04bb
Eric Fuller <efuller82@gmail.com>
2019-08-30 10:55:37 -0500

    pictures proper size

--- src/components/ToolCard/index.js
+++ src/components/ToolCard/index.js
@@ -3,7 +3,7 @@ import './style.css';
 
 function ToolCard(props) {
     return (
-        <div className='card'>
+        <div className='card img-card'>
             <div className='img-container'>
                 <img alt={props.id} src={props.image} />
             </div>
